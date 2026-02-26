2988a47670341c7fd54f6d6a72cd7d1408e66e42
Rachel <rachel.sun.rondon@gmail.com>
2026-02-26 13:00:30 -0500

    first commit

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import "./Toggle.css";
 
 const Toggle = () => {
 
-  const [isToggled, setIsToggled] = useState(false);
+  const [theme, setTheme] = useState("day")
+  const className = `layout ${theme}`;
+
+  const toggleTheme = (theme) => {
+    setTheme(theme);
+  };
 
   return (
-    <div>
-      {isToggled ? 
-      <button className="day" /> : <button className="night"/>}
+    <div className={className}>
+      <div className="buttons">
+        <button className="btn" value="day" onClick={((e) => toggleTheme(e.target.value))}>Day</button>
+        <button className="btn" value="auto" onClick={((e) => toggleTheme(e.target.value))}>Auto</button>
+        <button className="btn" value="night" onClick={((e) => toggleTheme(e.target.value))}>Night</button>
+      </div>
     </div>
   )
 };
