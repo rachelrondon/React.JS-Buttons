acdb975da01dbe2e05730a29a1eeefca8c4d4c01
Rachel <rachel.sun.rondon@gmail.com>
2026-02-27 17:32:16 -0500

    add color

--- src/Show.js
+++ src/Show.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { SunIcon, MoonIcon } from'@heroicons/react/24/outline';
 import './Show.css';
+
 
 const Show = () => {
 
-  return (
-    <div></div>
-  )
+
+ const [theme, setTheme] = useState("light");
+ const className = `layout ${theme}`;
+  const updateTheme = (theme) => {
+   setTheme(theme);
+ }
+
+
+ return (
+   <div className={className}>
+     <div className="buttons">
+       <button className="btn" value="light" onClick={((e) => updateTheme(e.currentTarget.value))}>
+         <SunIcon />
+         Light theme
+       </button>
+       <button className="btn" value="dark" onClick={((e) => updateTheme(e.currentTarget.value))}>
+         <MoonIcon />
+         Dark theme
+       </button>
+     </div>
+   </div>
+ )
 };
+
 
 export default Show;
