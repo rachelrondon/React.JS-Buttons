4844697432d772ff83e1b854d2d2730d2e6aa2aa
Rachel <rachel.sun.rondon@gmail.com>
2026-02-27 17:33:14 -0500

    feat: update

--- src/Show.js
+++ src/Show.js
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-import { SunIcon, MoonIcon } from'@heroicons/react/24/outline';
 import './Show.css';
-
 
 const Show = () => {
 
-
- const [theme, setTheme] = useState("light");
- const className = `layout ${theme}`;
-  const updateTheme = (theme) => {
-   setTheme(theme);
- }
-
-
  return (
-   <div className={className}>
-     <div className="buttons">
-       <button className="btn" value="light" onClick={((e) => updateTheme(e.currentTarget.value))}>
-         <SunIcon />
-         Light theme
-       </button>
-       <button className="btn" value="dark" onClick={((e) => updateTheme(e.currentTarget.value))}>
-         <MoonIcon />
-         Dark theme
-       </button>
-     </div>
-   </div>
+   <div></div>
  )
 };
-
 
 export default Show;
